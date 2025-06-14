ede34b692c80e9901413af6440e455f1131f3c6a
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-06-13 23:41:24 -0300

    Adicionando solução

---
+++ Screenmovie/Screenmovie/Models/Filme.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Screenmovie.Models
+{
+    internal class Filme
+    {
+        [JsonPropertyName("title")]
+        public string Nome { get; set; }
+
+        [JsonPropertyName("year")]
+        public string Ano { get; set; }
+
+
+        [JsonPropertyName("imDbRating")]
+        public string Nota { get; set; }
+
+        public void ExibirDetalhesDoFilme()
+        {
+            Console.WriteLine($"Titulo: {Nome}");
+            Console.WriteLine($"Ano de lançamento: {Ano}");
+            Console.WriteLine($"Nota no imDb: {Nota}");
+
+        }
+    }
+
+}
---
+++ Screenmovie/Screenmovie/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Screenmovie.Models;
+
+using (HttpClient client = new HttpClient())
+
+    try
+    {
+        string resposta = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+        var filmes = System.Text.Json.JsonSerializer.Deserialize<List<Filme>>(resposta);
+
+        filmes[0].ExibirDetalhesDoFilme();
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
